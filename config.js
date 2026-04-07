1e2d10912b115cf2dd162967272fada1ac88d5e3
rabimaruuu <124451636+rabimaruuu@users.noreply.github.com>
2026-04-08 07:32:01 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 window.APP_CONFIG = {
-  GAS_URL: "https://script.google.com/macros/s/AKfycbzSlglWYwjooOP5jOt9MCYS9FSWEAkUvgrc57z6JYoXfSQiSxgFarcaxU4tyOExXwQZ/exec"
+  GAS_URL: "https://script.google.com/macros/s/AKfycbwx22I7DVii1S_6gN43U82b2uKeg1CR4n8yMJtSoLrGgP8W2AkbNA3zpGrIbhjzGt6b/exec"
 };
