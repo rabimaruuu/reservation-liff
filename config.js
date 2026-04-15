43579bc81259ea01e14f3e266a05a041fa00ddf8
rabimaruuu <124451636+rabimaruuu@users.noreply.github.com>
2026-04-16 07:55:20 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 window.APP_CONFIG = {
-    API_URL: "https://asia-northeast1-reservation-system-492915.cloudfunctions.net/api"
+    API_URL: "https://asia-northeast1-reservation-system-492915.cloudfunctions.net/api",
+    LIFF_ID: "2009690638-qEYZlp9U"
 };
